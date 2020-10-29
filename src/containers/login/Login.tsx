6f7913ca8f8e7106c3f100647b78c91ff334efdf
SofieDunt <sofiedunt@gmail.com>
2020-10-29 19:59:03 -0400

    Create Login/Signup Pages

---
+++ src/containers/login/Login.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import './login.less';
+import {Button, Col, Form, Input, Row, Typography} from 'antd';
+import { login } from '../../auth/authAPI';
+import {Link} from 'react-router-dom';
+/*const {  } = Typography;*/
+
+
+const Login: React.FC = () => {
+  const onFinish = (values: any) => {
+    login({ email: values.username, password: values.password })
+  };
+
+  return (
+      <>
+        <div className="body-content-container">
+          <Row>
+            <Col span={10} className="input-container">
+
+                <h1>Log In</h1>
+                <hr/>
+                <Form
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                  <Row id="inputs">
+                    <Col className="leftInput">
+                      <Form.Item
+                          name="username"
+                          rules={[{ required: true, message: 'Please input your username!' }]}
+                      >
+                        <Input placeholder="Username"/>
+                      </Form.Item>
+                    </Col>
+                    <Col>
+                      <Form.Item
+                          name="password"
+                          rules={[{ required: true, message: 'Please input your password!' }]}
+                      >
+                        <Input.Password placeholder="Password"/>
+                      </Form.Item>
+                    </Col>
+                  </Row>
+
+                  <Form.Item id={'loginButton'} style={{ marginBottom: '10px' }}>
+                    <Button type="primary" htmlType="submit" size={'large'}
+                            style={{ backgroundColor: '#9AC356' , borderColor: '#9AC356'}}>
+                      Log In
+                    </Button>
+                  </Form.Item>
+
+                </Form>
+
+                <p id="forgotPassword"><Link to="/" component={Typography.Link} className="Link">
+                  Forgot Password?</Link></p>
+
+                <p>New to speak for the trees?</p>
+                <p>Sign up <Link className="Link" to="/signup" component={Typography.Link}>here!</Link></p>
+
+            </Col>
+
+            <Col span={2}></Col>
+
+            <Col span={12} className="info-container">
+                <h1>Welcome Back!</h1>
+                <p>Dreamcatcher kogi taiyaki keytar. Swag typewriter craft beer cronut pok pok gentrify flannel salvia
+                  deep v pork belly pitchfork. Swag fashion axe fam. Occupy biodiesel jean shorts affogato PBR&B freegan
+                  bushwick vegan four loko pickled.</p>
+            </Col>
+          </Row>
+        </div>
+      </>
+  );
+};
+
+export default Login;
---
+++ src/containers/signup/Signup.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import './signup.less';
+import {Button, Col, Form, Input, Row, Typography} from 'antd';
+import { signup } from '../../auth/authAPI';
+import {Link} from 'react-router-dom';
+/*const {  } = Typography;*/
+
+const rSpan = 8;
+const passSpan = 17;
+
+const Signup: React.FC = () => {
+    const onFinish = (values: any) => {
+        signup({ email: values.username, password: values.password })
+    };
+
+    return (
+        <>
+            <div className="body-content-container">
+                <Row>
+                    <Col span={10} className="input-container">
+                        <h1>Sign Up</h1>
+                        <hr/>
+                        <Form
+                            name="basic"
+                            initialValues={{ remember: true }}
+                            onFinish={onFinish}
+                        >
+                            <Row>
+                                <Col className="leftInput" span={rSpan}>
+                                    <Form.Item
+                                        name="fname"
+                                        rules={[{ required: true, message: 'Please input your first name!' }]}
+                                    >
+                                        <Input placeholder="First Name"/>
+                                    </Form.Item>
+                                </Col>
+                                <Col span={rSpan}>
+                                    <Form.Item
+                                        name="lname"
+                                        rules={[{ required: true, message: 'Please input your last name!' }]}
+                                    >
+                                        <Input placeholder="Last Name"/>
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col className="leftInput" span={rSpan}>
+                                    <Form.Item
+                                        name="email"
+                                        rules={[{ required: true, message: 'Please input your email!' }]}
+                                    >
+                                        <Input placeholder="Email"/>
+                                    </Form.Item>
+                                </Col>
+                                <Col span={rSpan}>
+                                    <Form.Item
+                                        name="cemail"
+                                        rules={[{ required: true, message: 'Please confirm your email!' }]}
+                                    >
+                                        <Input placeholder="Confirm Email"/>
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col span={passSpan}>
+                                    <Form.Item
+                                        name="password"
+                                        rules ={[{ required: true, message: 'Please enter a password!' }]}
+                                    >
+                                        <Input.Password placeholder="Password"/>
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={passSpan}>
+                                    <Form.Item
+                                        name="cpassword"
+                                        rules ={[{ required: true, message: 'Please confirm your password!' }]}
+                                    >
+                                        <Input.Password placeholder="Confirm Password"/>
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+
+                            <Row style={{ marginTop: '15px' }}>
+                                <Col style={{ marginRight: '15px' }}>
+                                    <Form.Item id={'loginButton'}>
+                                        <Button type="primary" htmlType="submit"
+                                                size={'large'} style={{ backgroundColor: '#9AC356' , borderColor: '#9AC356' }}>
+                                            Log In
+                                        </Button>
+                                    </Form.Item>
+                                </Col>
+                                <Col style={{ paddingTop: '5px' }}>
+                                    <p>Already have an account?</p>
+                                    <p>Log in <Link to="/login" component={Typography.Link} className="Link">here!</Link></p>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+
+                    <Col span={2}></Col>
+
+                    <Col span={12} className="info-container">
+                        <h1>Nice to meet you!</h1>
+                        <p>Dreamcatcher kogi taiyaki keytar. Swag typewriter craft beer cronut pok pok gentrify flannel salvia
+                                deep v pork belly pitchfork. Swag fashion axe fam. Occupy biodiesel jean shorts affogato PBR&B freegan
+                                bushwick vegan four loko pickled.</p>
+                    </Col>
+                </Row>
+            </div>
+        </>
+    );
+};
+
+export default Signup;
