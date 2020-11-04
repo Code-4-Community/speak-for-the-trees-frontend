2297df18bf240577155f8a91863f64ddadc8c704
SofieDunt <sofiedunt@gmail.com>
2020-11-04 16:26:08 -0500

    Create pageheader component

---
+++ src/components/pageheader/PageHeader.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './pageheader.less';
+import { Typography } from 'antd';
+const { Title, Paragraph } = Typography;
+
+type PageHeaderProps = {
+  readonly pageTitle: string;
+  readonly pageSubtitle: string;
+  readonly subtitleColor: string;
+};
+
+const PageHeader: React.FC<PageHeaderProps> = ({
+  pageTitle,
+  pageSubtitle,
+  subtitleColor,
+}) => {
+  return (
+    <div className="header">
+      <Title id="title">{pageTitle}</Title>
+      <Paragraph className="subtitle" id={subtitleColor}>
+        {pageSubtitle}
+      </Paragraph>
+    </div>
+  );
+};
+
+export default PageHeader;
