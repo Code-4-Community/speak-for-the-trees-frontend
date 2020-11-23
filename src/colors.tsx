5f8cdc458a4b4267e7c3e0c2429f871074378d7f
Sofie Duntugan <sofiedunt@gmail.com>
2020-11-22 19:56:41 -0500

    Created user home page

--- src/colors.tsx
+++ src/colors.tsx
@@ -3,3 +3,6 @@ export const LINK = '#1890ff';
 export const BLACK = '#000000';
 export const TEXT_GREY = '#AFAEAE';
 export const DARKGREY = '#444444';
+export const MID_GREEN = '#61802E';
+export const LIGHT_GREEN = '#9AC356';
+export const WHITE = '#FFFFFF';
