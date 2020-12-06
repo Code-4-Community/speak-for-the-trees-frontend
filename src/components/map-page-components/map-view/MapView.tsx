65aa310483d05d0374bc25e53237a527c40380ab
unknown <florisdobber@gmail.com>
2020-12-06 12:39:43 -0500

    There's a map!!!

--- src/components/map-page-components/map-view/MapView.tsx
+++ src/components/map-page-components/map-view/MapView.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+// import { } from 'googlemaps'
+import { Loader } from '@googlemaps/js-api-loader';
+import styled from 'styled-components';
+
+let map: google.maps.Map;
+
+const loader = new Loader({
+  apiKey: 'AIzaSyBNSl4FTHGzPGsYMmZnDqMkCtAVbMIg6bg',
+  version: 'weekly',
+});
+
+loader.load().then(() => {
+  map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+});
+
+const MapDiv = styled.div`
+  height: 100%;
+`;
 
 const MapView: React.FC = () => {
-  return <div>This is a placeholder</div>;
+  // useEffect(() => {
+  //   if (!document.getElementById("googleMapScript")) {
+  //     const googleMapScript = document.createElement('script')
+  //     googleMapScript.id = "googleMapScript"
+  //     googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAP_API}&libraries=places`
+  //     window.document.body.appendChild(googleMapScript)
+  //   }
+  // },[])
+  // useEffect(initMap);
+  return (
+    <>
+      {/* <div id="pac-container">
+        <StyledSearch id="pac-input" placeholder="Address" />
+      </div> */}
+      <MapDiv id="map"></MapDiv>
+    </>
+  );
 };
 
 export default MapView;
