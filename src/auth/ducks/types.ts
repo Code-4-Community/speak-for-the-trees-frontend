9b25e49e66a60e874d20dc1edb9805f79215cbf8
Aaron Pradhan <12a.pradhan@gmail.com>
2021-01-31 11:36:19 -0500

    Improve signup page

--- src/auth/ducks/types.ts
+++ src/auth/ducks/types.ts
@@ -28,6 +28,7 @@ export interface LoginRequest {
 
 export interface SignupRequest {
   readonly email: string;
+  readonly username: string;
   readonly password: string;
   readonly firstName: string;
   readonly lastName: string;
