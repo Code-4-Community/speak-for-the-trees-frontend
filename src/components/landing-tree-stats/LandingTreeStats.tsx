c7710617decb7f6a8b953f5bb726e1dbc12947e5
Sofie Duntugan <sofiedunt@gmail.com>
2021-01-31 02:51:02 -0500

    Optimize landing stats for mobile, use styled components

--- src/components/landing-tree-stats/LandingTreeStats.tsx
+++ src/components/landing-tree-stats/LandingTreeStats.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Typography } from 'antd';
+import useWindowDimensions, { WindowTypes } from '../window-dimensions';
 import InfoCard from '../info-card/InfoCard';
-import './landing-tree-stats.less';
+import MobileInfoCard from '../mobile-info-card/MobileInfoCard';
+import { TEXT_GREY, MID_GREEN } from '../../colors';
+
+const { Title, Paragraph, Link } = Typography;
+
+const TreeStatsContainer = styled.div`
+  margin-top: 35px;
+`;
+
+const MapCardsContainer = styled.div`
+  margin: 30px auto;
+`;
+
+const MapCard = styled.div`
+  margin: 10px auto;
+`;
+
+const MobileTitle = styled(Paragraph)`
+  color: ${MID_GREEN};
+  font-size: 14px;
+`;
+
+const MobileMapCardsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 10px auto;
+`;
+
+const RightMargin = styled.div`
+  margin-right: 10px;
+`;
+
+const GreyParagraph = styled(Paragraph)`
+  color: ${TEXT_GREY};
+`;
 
 type LandingTreeStatsProps = {
   readonly moneySaved: number;
@@ -14,33 +50,76 @@ const LandingTreeStats: React.FC<LandingTreeStatsProps> = ({
   rainWater,
   carbonEmissions,
 }) => {
-  return (
-    <div className="tree-stats-container">
-      <Typography.Title level={3}>Current Status of our Trees</Typography.Title>
+  const { windowType } = useWindowDimensions();
 
-      <div className="map-cards-container">
-        <div className="map-card">
-          <InfoCard header="Money Saved" body={getMoneyString(moneySaved)} />
-        </div>
+  switch (windowType) {
+    case WindowTypes.Mobile:
+      return (
+        <TreeStatsContainer>
+          <MobileTitle>Current Status of our Trees</MobileTitle>
+          <MobileMapCardsContainer>
+            <RightMargin>
+              <MobileInfoCard
+                header="Money Saved"
+                body={getMoneyString(moneySaved)}
+              />
+            </RightMargin>
 
-        <div className="map-card">
-          <InfoCard
-            header="Rain Water Caught"
-            body={`${rainWater.toLocaleString()} gallons`}
-          />
-        </div>
+            <RightMargin>
+              <MobileInfoCard
+                header="Rain Water Caught"
+                body={`${rainWater.toLocaleString()} gallons`}
+              />
+            </RightMargin>
 
-        <div className="map-card">
-          <InfoCard header="Carbon Emissions" body={`${carbonEmissions}%`} />
-        </div>
-      </div>
+            <MobileInfoCard
+              header="Carbon Emissions"
+              body={`${carbonEmissions}%`}
+            />
+          </MobileMapCardsContainer>
 
-      <Typography.Paragraph>
-        Learn more about how we got these numbers{' '}
-        <Typography.Link underline>here</Typography.Link>.
-      </Typography.Paragraph>
-    </div>
-  );
+          <GreyParagraph>
+            Learn more about how we got these numbers{' '}
+            <Link underline>here</Link>.
+          </GreyParagraph>
+        </TreeStatsContainer>
+      );
+
+    default:
+      return (
+        <TreeStatsContainer>
+          <Title level={3}>Current Status of our Trees</Title>
+
+          <MapCardsContainer>
+            <MapCard>
+              <InfoCard
+                header="Money Saved"
+                body={getMoneyString(moneySaved)}
+              />
+            </MapCard>
+
+            <MapCard>
+              <InfoCard
+                header="Rain Water Caught"
+                body={`${rainWater.toLocaleString()} gallons`}
+              />
+            </MapCard>
+
+            <MapCard>
+              <InfoCard
+                header="Carbon Emissions"
+                body={`${carbonEmissions}%`}
+              />
+            </MapCard>
+          </MapCardsContainer>
+
+          <Paragraph>
+            Learn more about how we got these numbers{' '}
+            <Link underline>here</Link>.
+          </Paragraph>
+        </TreeStatsContainer>
+      );
+  }
 };
 
 // TODO: Move function to a common utilities file
