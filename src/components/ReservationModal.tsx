76521ae3bd6d89c6dff1f1004c9655b2f81b5312
Vrushali Tarte <vrushalitarte@Vrushalis-MBP.hsd1.ca.comcast.net>
2021-02-21 11:46:20 -0800

    added finishing touches based on feedback

--- src/components/ReservationModal.tsx
+++ src/components/ReservationModal.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { WHITE, BLACK } from '../colors';
 import { Modal } from 'antd';
@@ -6,7 +6,7 @@ import { Modal } from 'antd';
 export enum ReservationModalType {
   OPEN = 'OPEN',
   TAKEN = 'TAKEN',
-  RESERVED = 'RESERVED'
+  RESERVED = 'RESERVED',
 }
 
 interface ReservationModalProps {
@@ -18,76 +18,69 @@ interface ReservationModalProps {
 }
 
 const StyledModal = styled(Modal)`
-    height: 40px;
-    background: ${WHITE};
-    color: ${BLACK};
-    font-size: 5px;
-    font-weight: 400;
+  height: 40px;
+  background: ${WHITE};
+  color: ${BLACK};
+  font-size: 5px;
+  font-weight: 400;
 `;
 
-const ReservationModal: React.FC<ReservationModalProps> = ({ 
-  status, blockID, onOk, onCancel, isVisible}) => {
-    const [visibility, setVisibility] = useState(isVisible);
-
-    const getOkText = (): string => {
-      switch (status) {
-        case ReservationModalType.OPEN : 
-          return 'Reserve'
-        case ReservationModalType.TAKEN : 
-          return 'Release'
-        case ReservationModalType.RESERVED : 
-          return 'Ok'
-      }
-    };
-
-    const handleCancel = (): void => {
-      onCancel();
-      setVisibility(false);
+const ReservationModal: React.FC<ReservationModalProps> = ({
+  status,
+  blockID,
+  onOk,
+  onCancel,
+  isVisible,
+}) => {
+  const getOkText = (): string => {
+    switch (status) {
+      case ReservationModalType.OPEN:
+        return 'Reserve';
+      case ReservationModalType.TAKEN:
+        return 'Release';
+      case ReservationModalType.RESERVED:
+        return 'Ok';
     }
+  };
 
-    const handleOk = (): void => {
-      onOk();
-      setVisibility(false);
+  const getModalContent = () => {
+    switch (status) {
+      case ReservationModalType.OPEN:
+        return (
+          <text>
+            <b>{`Are you sure?`}</b>
+            <br /> {`You want to reserve block ${blockID}`}
+          </text>
+        );
+      case ReservationModalType.TAKEN:
+        return (
+          <text>
+            <b>{`Are you sure?`}</b>
+            <br /> {`You want to release block ${blockID}`}
+          </text>
+        );
+      case ReservationModalType.RESERVED:
+        return (
+          <text>
+            <b>{`Sorry!`}</b>
+            <br /> {`Block ${blockID} is not available to reserve/release`}
+          </text>
+        );
     }
+  };
 
-    const getModalContent = () => {
-      switch (status) {
-        case ReservationModalType.OPEN : 
-          return (
-            <text>
-              <b>{`Are you sure?`}</b><br/ > {`You want to reserve block ${blockID}`}
-            </text>
-          );
-        case ReservationModalType.TAKEN : 
-          return (
-            <text>
-              <b>{`Are you sure?`}</b><br/ > {`You want to release block ${blockID}`}
-            </text>
-          );
-        case ReservationModalType.RESERVED : 
-          return (
-              <text>
-                <b>{`Sorry!`}</b><br/ > {`Block ${blockID} is not available to reserve/release`}
-              </text>
-          );
-      }
-    };
-
-    return (
-      <div>
-        <StyledModal
-          visible={visibility}
-          title = ''
-          onOk={handleOk}
-          okText={getOkText()}
-          onCancel={handleCancel}
-          closable={false}
-        >
-          {getModalContent()}
-        </StyledModal>
-      </div>
-    );
+  return (
+    <StyledModal
+      visible={isVisible}
+      title=""
+      onOk={onOk}
+      okText={getOkText()}
+      onCancel={onCancel}
+      closable={false}
+    >
+      {getModalContent()}
+    </StyledModal>
+  );
 };
 
-
-export default ReservationModal
+export default ReservationModal;
