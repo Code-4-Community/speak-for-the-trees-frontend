536e631458393fbbcff230606edecb67860d4c8d
willmt80 <willmt80@gmail.com>
2021-02-21 22:03:10 -0500

    added settings routes, connected to settings page, and wrote tests for non-error cases

--- src/api/protectedApiClient.ts
+++ src/api/protectedApiClient.ts
@@ -5,10 +5,17 @@ export interface ProtectedApiClient {
     currentPassword: string;
     newPassword: string;
   }) => Promise<void>;
+  readonly changeUsername: (request: {
+    newUsername: string;
+    password: string;
+  }) => Promise<void>;
+  readonly deleteUser: (request: { password: string }) => Promise<void>;
 }
 
 enum ProtectedApiClientRoutes {
   CHANGE_PASSWORD = '/api/v1/protected/user/change_password',
+  CHANGE_USERNAME = '/api/v1/protected/user/change_username',
+  DELETE_USER = '/api/v1/protected/user/delete',
 }
 
 const changePassword = (request: {
@@ -23,8 +30,28 @@ const changePassword = (request: {
     .catch((e) => e);
 };
 
+const changeUsername = (request: {
+  newUsername: string;
+  password: string;
+}): Promise<void> => {
+  return AppAxiosInstance.post(
+    ProtectedApiClientRoutes.CHANGE_USERNAME,
+    request,
+  )
+    .then((r) => r)
+    .catch((e) => e);
+};
+
+const deleteUser = (request: { password: string }): Promise<void> => {
+  return AppAxiosInstance.post(ProtectedApiClientRoutes.DELETE_USER, request)
+    .then((r) => r)
+    .catch((e) => e);
+};
+
 const Client: ProtectedApiClient = Object.freeze({
   changePassword,
+  changeUsername,
+  deleteUser,
 });
 
 export default Client;
