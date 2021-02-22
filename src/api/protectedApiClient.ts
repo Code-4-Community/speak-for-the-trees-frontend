cdaa6ddadf8d36cc28e3ddd113d27c541ffbfacf
willmt80 <willmt80@gmail.com>
2021-02-21 23:27:11 -0500

    added reservation route tests

--- src/api/protectedApiClient.ts
+++ src/api/protectedApiClient.ts
@@ -4,7 +4,7 @@ export interface ProtectedApiClient {
   readonly makeReservation: (blockId: number, teamId?: number) => Promise<void>;
   readonly completeReservation: (
     blockId: number,
-    teamId: number,
+    teamId?: number,
   ) => Promise<void>;
   readonly releaseReservation: (blockId: number) => Promise<void>;
   readonly uncompleteReservation: (blockId: number) => Promise<void>;
@@ -24,7 +24,7 @@ export enum ProtectedApiClientRoutes {
   CHANGE_PASSWORD = '/api/v1/protected/user/change_password',
 }
 
-enum AdminApiClientRoutes {
+export enum AdminApiClientRoutes {
   UNCOMPLETE_RESERVATION = '/api/v1/protected/reservations/uncomplete',
   MARK_RESERVATION_FOR_QA = '/api/v1/protected/reservations/qa',
   PASS_RESERVATION_QA = '/api/v1/protected/reservations/pass_qa',
@@ -35,9 +35,9 @@ const makeReservation = (blockId: number, teamId?: number): Promise<void> => {
   return AppAxiosInstance.post(ProtectedApiClientRoutes.MAKE_RESERVATION, {
     block_id: blockId,
     team_id: teamId,
-  }).then(
-    (response) => response.data,
-  );
+  })
+    .then((r) => r.data)
+    .catch((e) => e);
 };
 
 const completeReservation = (
@@ -47,13 +47,17 @@ const completeReservation = (
   return AppAxiosInstance.post(ProtectedApiClientRoutes.COMPLETE_RESERVATION, {
     block_id: blockId,
     team_id: teamId,
-  });
+  })
+    .then((r) => r.data)
+    .catch((e) => e);
 };
 
 const releaseReservation = (blockId: number): Promise<void> => {
   return AppAxiosInstance.post(ProtectedApiClientRoutes.RELEASE_RESERVATION, {
     block_id: blockId,
-  });
+  })
+    .then((r) => r.data)
+    .catch((e) => e);
 };
 
 // Admin routes
@@ -61,25 +65,33 @@ const releaseReservation = (blockId: number): Promise<void> => {
 const uncompleteReservation = (blockId: number): Promise<void> => {
   return AppAxiosInstance.post(AdminApiClientRoutes.UNCOMPLETE_RESERVATION, {
     block_id: blockId,
-  });
+  })
+    .then((r) => r.data)
+    .catch((e) => e);
 };
 
 const markReservationForQa = (blockId: number): Promise<void> => {
   return AppAxiosInstance.post(AdminApiClientRoutes.MARK_RESERVATION_FOR_QA, {
     block_id: blockId,
-  });
+  })
+    .then((r) => r.data)
+    .catch((e) => e);
 };
 
 const passReservationQa = (blockId: number): Promise<void> => {
   return AppAxiosInstance.post(AdminApiClientRoutes.PASS_RESERVATION_QA, {
     block_id: blockId,
-  });
+  })
+    .then((r) => r.data)
+    .catch((e) => e);
 };
 
 const failReservationQa = (blockId: number): Promise<void> => {
   return AppAxiosInstance.post(AdminApiClientRoutes.FAIL_RESERVATION_QA, {
     block_id: blockId,
-  });
+  })
+    .then((r) => r.data)
+    .catch((e) => e);
 };
 
 const changePassword = (request: {
@@ -102,7 +114,7 @@ const Client: ProtectedApiClient = Object.freeze({
   markReservationForQa,
   passReservationQa,
   failReservationQa,
-  changePassword
+  changePassword,
 });
 
 export default Client;
--- src/api/test/protectedApiClient.test.ts
+++ src/api/test/protectedApiClient.test.ts
@@ -1,5 +1,8 @@
 import nock from 'nock';
-import ProtectedApiClient, { ProtectedApiClientRoutes } from '../protectedApiClient';
+import ProtectedApiClient, {
+  ProtectedApiClientRoutes,
+  AdminApiClientRoutes,
+} from '../protectedApiClient';
 
 const BASE_URL = 'http://localhost';
 
@@ -7,14 +10,126 @@ const BASE_URL = 'http://localhost';
 
 describe('Protected Client Tests', () => {
   describe('makeReservation', () => {
-    it('makes the right request', async () => {
-      const response = "";
+    it('makes the right request with team', async () => {
+      const response = '';
 
-      nock(BASE_URL).post(ProtectedApiClientRoutes.MAKE_RESERVATION).reply(200, response);
+      nock(BASE_URL)
+        .post(ProtectedApiClientRoutes.MAKE_RESERVATION)
+        .reply(200, response);
+
+      const result = await ProtectedApiClient.makeReservation(1, 1);
+
+      expect(result).toEqual(response);
+    });
+
+    it('makes the right request without team', async () => {
+      const response = '';
+
+      nock(BASE_URL)
+        .post(ProtectedApiClientRoutes.MAKE_RESERVATION)
+        .reply(200, response);
 
       const result = await ProtectedApiClient.makeReservation(1);
 
       expect(result).toEqual(response);
+    });
+  });
+
+  describe('completeReservation', () => {
+    it('makes the right request with team', async () => {
+      const response = '';
+
+      nock(BASE_URL)
+        .post(ProtectedApiClientRoutes.COMPLETE_RESERVATION)
+        .reply(200, response);
+
+      const result = await ProtectedApiClient.completeReservation(2, 2);
+
+      expect(result).toEqual(response);
+    });
+
+    it('makes the right request without team', async () => {
+      const response = '';
+
+      nock(BASE_URL)
+        .post(ProtectedApiClientRoutes.COMPLETE_RESERVATION)
+        .reply(200, response);
+
+      const result = await ProtectedApiClient.completeReservation(2);
+
+      expect(result).toEqual(response);
+    });
+  });
+
+  describe('releaseReservation', () => {
+    it('makes the right request', async () => {
+      const response = '';
+
+      nock(BASE_URL)
+        .post(ProtectedApiClientRoutes.RELEASE_RESERVATION)
+        .reply(200, response);
+
+      const result = await ProtectedApiClient.releaseReservation(2);
+
+      expect(result).toEqual(response);
+    });
+  });
+});
+
+describe('Admin Protected Client Routes', () => {
+  describe('uncompleteReservation', () => {
+    it('makes the right request', async () => {
+      const response = '';
+
+      nock(BASE_URL)
+        .post(AdminApiClientRoutes.UNCOMPLETE_RESERVATION)
+        .reply(200, response);
+
+      const result = await ProtectedApiClient.uncompleteReservation(5);
+
+      expect(result).toEqual(response);
+    });
+  });
+
+  describe('markReservationForQa', () => {
+    it('makes the right request', async () => {
+      const response = '';
+
+      nock(BASE_URL)
+        .post(AdminApiClientRoutes.MARK_RESERVATION_FOR_QA)
+        .reply(200, response);
+
+      const result = await ProtectedApiClient.markReservationForQa(6);
+
+      expect(result).toEqual(response);
+    });
+  });
+
+  describe('passReservationQa', () => {
+    it('makes the right request', async () => {
+      const response = '';
+
+      nock(BASE_URL)
+        .post(AdminApiClientRoutes.PASS_RESERVATION_QA)
+        .reply(200, response);
+
+      const result = await ProtectedApiClient.passReservationQa(6);
+
+      expect(result).toEqual(response);
+    });
+  });
+
+  /*
+  describe('failReservationQa', () => {
+    it('makes the right request', async () => {
+      const response = "";
+
+      nock(BASE_URL).post(AdminApiClientRoutes.PASS_RESERVATION_QA).reply(200, response);
+
+      const result = await ProtectedApiClient.failReservationQa(7);
+
+      expect(result).toEqual(response);
     }) 
-  });    
+  });
+  */
 });
