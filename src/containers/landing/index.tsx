9cc575420e2bffcc34e9642972f4330364ae4aa1
willmt80 <willmt80@gmail.com>
2021-03-08 11:43:10 -0500

    linting and prettier

--- src/containers/landing/index.tsx
+++ src/containers/landing/index.tsx
@@ -9,7 +9,9 @@ import LandingTreeStats from '../../components/landingTreeStats';
 import useWindowDimensions, {
   WindowTypes,
 } from '../../components/window-dimensions';
-import { AsyncRequestKinds, asyncRequestIsComplete } from '../../utils/asyncRequest';
+import {
+  asyncRequestIsComplete,
+} from '../../utils/asyncRequest';
 import { getMapGeoData } from '../../components/mapPageComponents/ducks/thunks';
 import { MapGeoDataReducerState } from '../../components/mapPageComponents/ducks/types';
 import { C4CState } from '../../store';
@@ -45,8 +47,8 @@ const Landing: React.FC<LandingProps> = ({ neighborhoods, blocks }) => {
               content="The first page someone sees if they are not logged in, contains a read only map of Boston neighborhoods and some information about the tree counts of Speak for the Trees"
             />
           </Helmet>
-          {(asyncRequestIsComplete(blocks) &&
-            asyncRequestIsComplete(neighborhoods)) && (
+          {asyncRequestIsComplete(blocks) &&
+            asyncRequestIsComplete(neighborhoods) && (
               <MobileMapPage
                 neighborhoods={neighborhoods.result}
                 blocks={blocks.result}
@@ -75,8 +77,8 @@ const Landing: React.FC<LandingProps> = ({ neighborhoods, blocks }) => {
               content="The first page someone sees if they are not logged in, contains a read only map of Boston neighborhoods and some information about the tree counts of Speak for the Trees"
             />
           </Helmet>
-          {(asyncRequestIsComplete(blocks) &&
-            asyncRequestIsComplete(neighborhoods)) && (
+          {asyncRequestIsComplete(blocks) &&
+            asyncRequestIsComplete(neighborhoods) && (
               <MapPage
                 neighborhoods={neighborhoods.result}
                 blocks={blocks.result}
