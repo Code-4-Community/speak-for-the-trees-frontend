b87faa39ecdcb70eb0ae6d810dd22d3fa987d0d8
Liam Moynihan <liammoyn@gmail.com>
2021-03-30 17:20:36 -0400

    Reduced footer padding and centered content

--- src/components/siteFooter/index.tsx
+++ src/components/siteFooter/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Layout, Typography } from 'antd';
 import { BugOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
 
 const { Footer } = Layout;
 const { Text, Link } = Typography;
 
+const StyledFooter = styled(Footer)`
+  padding-top: 12px;
+  padding-bottom: 12px;
+  margin: auto;
+`;
+
 const SiteFooter: React.FC = () => {
   return (
     <>
-      <Footer>
+      <StyledFooter>
         <BugOutlined />
         <Text> Notice something off? </Text>
         <Link href="https://forms.gle/4dLLYX4z5MMsgaAq7" target="_blank">
           Submit a Bug Report!
         </Link>
-      </Footer>
+      </StyledFooter>
     </>
   );
 };
