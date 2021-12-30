bdc13475b7ac0a7378636cfc7f756e5b39a5fab1
Sofie Duntugan <sofiedunt@gmail.com>
2021-12-30 04:40:18 -0800

    Add report routes to api

---
+++ src/containers/reports/ducks/types.ts
@@ -0,0 +1,30 @@
+export interface AdoptionReport {
+  readonly adoptionReport: AdoptionReportEntry[];
+}
+
+export interface AdoptionReportEntry {
+  readonly siteId: number;
+  readonly address: string;
+  readonly name: string;
+  readonly email: string;
+  readonly dateAdopted: Date;
+  readonly activityCount: number;
+  readonly neighborhood: string;
+}
+
+export interface StewardshipReport {
+  readonly stewardshipReport: StewardshipReportEntry[];
+}
+
+export interface StewardshipReportEntry {
+  readonly siteId: number;
+  readonly address: string;
+  readonly name: string;
+  readonly email: string;
+  readonly datePerformed: Date;
+  readonly watered: boolean;
+  readonly mulched: boolean;
+  readonly cleaned: boolean;
+  readonly weeded: boolean;
+  readonly neighborhood: string;
+}
