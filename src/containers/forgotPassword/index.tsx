ed496449ba398ec7bfcd4e5408e92f3c3ad80a30
Kenny Jung <dukyoung01@naver.com>
2022-03-20 15:34:58 -0400

    Improve forgot password page

--- src/containers/forgotPassword/index.tsx
+++ src/containers/forgotPassword/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import authClient from '../../auth/authClient';
 import { ForgotPasswordRequest } from '../../auth/ducks/types';
 import useWindowDimensions from '../../components/windowDimensions';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Typography } from 'antd';
 import PageHeader from '../../components/pageHeader';
 import { ContentContainer } from '../../components/themedComponents';
 import { isMobile } from '../../utils/isCheck';
@@ -12,16 +12,23 @@ import { enterEmailRules } from '../../utils/formRules';
 const ForgotPassword: React.FC = () => {
   const { windowType } = useWindowDimensions();
 
+  const [submittedEmail, setSubmittedEmail] = useState(false);
+  const [userNotFound, setUserNotFound] = useState(false);
+  const [form] = Form.useForm();
+
   const onFinish = (values: ForgotPasswordRequest) => {
     authClient
       .forgotPassword(values)
       .then(() => {
-        alert('Successfully sent forgot password request!');
+        setSubmittedEmail(true);
+        setUserNotFound(false);
       })
-      .catch((err) => {
-        alert('Forgot password request unsuccessful!');
+      .catch(() => {
+        setUserNotFound(true);
+        form.resetFields();
       });
   };
+
   return (
     <>
       <Helmet>
@@ -33,19 +40,39 @@ const ForgotPassword: React.FC = () => {
       </Helmet>
       <ContentContainer>
         <PageHeader
-          pageTitle="Forgot Password"
+          pageTitle="Forgot your password?"
           isMobile={isMobile(windowType)}
         />
-        <Form name="forgotPassword" onFinish={onFinish}>
-          <Form.Item name="email" rules={enterEmailRules}>
-            <Input placeholder="Email" />
-          </Form.Item>
-          <Form.Item>
-            <Button type="primary" htmlType="submit" size="large">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+        {!submittedEmail || userNotFound ? (
+          <>
+            <Typography.Title level={4}>
+              Please enter your email and we will send you the password reset
+              instructions to the email address for this account.
+            </Typography.Title>
+            <Form form={form} name="forgotPassword" onFinish={onFinish}>
+              {userNotFound && (
+                <Typography.Title level={4}>
+                  We couldn't find an account associated with the given email.
+                  Please try again.
+                </Typography.Title>
+              )}
+              <Form.Item name="email" rules={enterEmailRules}>
+                <Input placeholder="Email" />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" size="large">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </>
+        ) : (
+          <>
+            <Typography.Title level={4}>
+              The password reset instructions have been sent to your email!
+            </Typography.Title>
+          </>
+        )}
       </ContentContainer>
     </>
   );
