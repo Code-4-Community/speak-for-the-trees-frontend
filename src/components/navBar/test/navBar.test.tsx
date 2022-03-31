2584b939ae3f61a18213116f22ecb32b7cc9859f
Sofie Duntugan <sofiedunt@gmail.com>
2022-03-30 21:43:31 -0400

    Add navbar logout test

--- src/components/navBar/test/navBar.test.tsx
+++ src/components/navBar/test/navBar.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from '../index';
+import NavMenu from '../navMenu';
 
 // window.matchMedia is invoked when rendering the NavBar page, but is not implemented in JSDOM, so we mock it here
 // see more: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
@@ -45,5 +46,20 @@ describe('NavBar', () => {
     );
 
     expect(screen.queryByText('First Last')).toBeInTheDocument();
+  });
+
+  it('logs out on click', () => {
+    const mockLogout = jest.fn();
+
+    // render the nav menu
+    render(
+      <BrowserRouter>
+        <NavMenu isAdmin={false} onLogout={mockLogout} />
+      </BrowserRouter>,
+    );
+
+    screen.getByText('Log Out').click();
+
+    expect(mockLogout).toHaveBeenCalledTimes(1);
   });
 });
