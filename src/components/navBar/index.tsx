82524707fb4d9487b921e974488907b34d2eb764
Sofie Duntugan <sofiedunt@gmail.com>
2022-05-01 02:47:44 -0400

    Create welcome modal

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Routes } from '../../App';
 import styled from 'styled-components';
-import { Col, Row } from 'antd';
 import useWindowDimensions, { WindowTypes } from '../windowDimensions';
 import MobileNavBar from './mobileNavBar';
 import { BACKGROUND_GREY, MID_GREEN } from '../../utils/colors';
-import sfttLogo from '../../assets/images/sfttNameLogo.png';
-import bostonLogo from '../../assets/images/bostonParksLogo.png';
-import c4cLogo from '../../assets/images/c4cTextLogo.png';
 import { LinkButton } from '../linkButton';
 import NavExtra from './navExtra';
+import { SFTT_PARTNER_LOGOS } from '../../assets/links';
 
 const NavContainer = styled.div`
   box-shadow: '0 4px 2px -2px grey';
@@ -27,36 +24,12 @@ const MainLogo = styled.img`
   margin-right: 10px;
 `;
 
-const BostonLogo = styled.img`
-  height: 55px;
-  line-height: 0px;
-`;
-
-const C4CLogo = styled.img`
-  height: 27px;
-  line-height: 0px;
-`;
-
 const NoHoverShadeButton = styled(LinkButton)`
   height: 100%;
   overflow: hidden;
   &:hover {
     background: ${BACKGROUND_GREY};
   }
-`;
-
-const LogoCol = styled(Col)`
-  height: 100%;
-`;
-
-const NavTitleText = styled.div`
-  font-size: 14px;
-  font-weight: bold;
-  height: 100%;
-  display: inline-block;
-  color: ${MID_GREEN};
-  margin-bottom: 10px;
-  line-height: 0px;
 `;
 
 interface NavBarProps {
@@ -79,31 +52,9 @@ const NavBar: React.FC<NavBarProps> = ({ userName, isAdmin, onLogout }) => {
   } else {
     return (
       <NavContainer>
-        {windowType === WindowTypes.Desktop ? (
-          <NoHoverShadeButton type="text" to={Routes.HOME}>
-            <Row align="middle">
-              <Col span={6}>
-                <MainLogo src={sfttLogo} alt="icon" />
-              </Col>
-              <Col span={6}>
-                <NavTitleText>in partnership with</NavTitleText>
-              </Col>
-              <LogoCol span={4}>
-                <BostonLogo src={bostonLogo} />
-              </LogoCol>
-              <Col span={2}>
-                <NavTitleText>and</NavTitleText>
-              </Col>
-              <LogoCol span={4}>
-                <C4CLogo src={c4cLogo} />
-              </LogoCol>
-            </Row>
-          </NoHoverShadeButton>
-        ) : (
-          <NoHoverShadeButton type="text" to={Routes.HOME}>
-            <MainLogo src={sfttLogo} alt="icon" />
-          </NoHoverShadeButton>
-        )}
+        <NoHoverShadeButton type="text" to={Routes.HOME}>
+          <MainLogo src={SFTT_PARTNER_LOGOS} alt={'SFTT Logo'} />
+        </NoHoverShadeButton>
         <NavExtra userName={userName} isAdmin={isAdmin} onLogout={onLogout} />
       </NavContainer>
     );
---
+++ src/containers/landing/content.tsx
@@ -0,0 +1,16 @@
+// Welcome Modal
+export const MODAL_TITLE = {
+  ENG:
+    'Welcome to Speak for the Trees’ Adopt-A-Tree Program, run in partnership ' +
+    'with the Boston Parks and Recreation Department and Code4Community!',
+};
+export const MODAL_PARAGRAPH = {
+  ENG:
+    'Are there young, newly planted street trees near you? Explore our ' +
+    'tree map to learn how to adopt one (or a few!) and care for them to ' +
+    'help Boston’s young trees reach maturity and give your community ' +
+    'the many benefits mature trees provide.',
+};
+export const MODAL_OK_TEXT = {
+  ENG: 'Adopt a Tree!',
+};
