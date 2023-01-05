9055b82834ed26541c31d00d0d2969586955c743
Kenny Jung <dukyoung01@naver.com>
2023-01-04 22:16:50 -0800

    Save files

--- src/containers/login/test/login.test.tsx
+++ src/containers/login/test/login.test.tsx
@@ -22,7 +22,7 @@ import {
 // see more: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
-  value: jest.fn().mockImplementation((query) => ({
+  value: (query: any) => ({
     matches: false,
     media: query,
     onchange: null,
@@ -31,7 +31,7 @@ Object.defineProperty(window, 'matchMedia', {
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
-  })),
+  }),
 });
 
 /*
