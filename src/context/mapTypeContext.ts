0b6fd76ba150177a8328c8f91ea54cee03dd79d8
Avery Huang <averyhuang2003@gmail.com>
2023-03-26 21:51:13 -0400

    default value for setMapTypeId

--- src/context/mapTypeContext.ts
+++ src/context/mapTypeContext.ts
@@ -3,8 +3,7 @@ import { MapTypes, SetStateType } from './types';
 
 export const MapTypeContext = React.createContext<
   [MapTypes, SetStateType<MapTypes>]
-  // @ts-ignore
->([MapTypes.ROADMAP, undefined]);
+>([MapTypes.ROADMAP, (value: React.SetStateAction<MapTypes>) => undefined]);
 
 export const useMapTypeContext = (): [MapTypes, SetStateType<MapTypes>] =>
   useContext(MapTypeContext);
