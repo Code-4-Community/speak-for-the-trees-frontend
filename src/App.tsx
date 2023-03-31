148ee7fa45fce413d92083d2a81aee62bcf96728
Kenny Jung <dukyoung01@naver.com>
2023-03-31 10:13:58 -0400

    Clean up code

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ import {
   PrivilegeLevel,
   UserAuthenticationReducerState,
 } from './auth/ducks/types';
-import { getPrivilegeLevel, isAdmin, getUserFullName } from './auth/ducks/selectors';
+import {
+  getPrivilegeLevel,
+  isAdmin,
+  getUserFullName,
+} from './auth/ducks/selectors';
 import { C4CState } from './store';
 
 import styled from 'styled-components';
@@ -95,7 +99,7 @@ const App: React.FC = () => {
     return getUserFullName(state.authenticationState.userData);
   });
 
-  const isAnAdmin: boolean = useSelector((state:C4CState) => {
+  const isAnAdmin: boolean = useSelector((state: C4CState) => {
     return isAdmin(state.authenticationState.tokens);
   });
 
