2dda26992952b8994104e4edd74b42efc5a258bd
Kenny Jung <dukyoung01@naver.com>
2023-03-31 10:40:06 -0400

    Revert small change

--- src/components/mapComponents/maps/mapWithPopup/index.tsx
+++ src/components/mapComponents/maps/mapWithPopup/index.tsx
@@ -45,9 +45,8 @@ const MapWithPopup: React.FC<MapWithPopupProps> = ({
   children,
 }) => {
   // BasicTreeInfo to display in tree popup
-  const [activeTreeInfo, setActiveTreeInfo] = useState<BasicTreeInfo>(
-    defaultActiveTree,
-  );
+  const [activeTreeInfo, setActiveTreeInfo] =
+    useState<BasicTreeInfo>(defaultActiveTree);
 
   const initMapCallback = useCallback(initMap, []);
 
@@ -60,8 +59,6 @@ const MapWithPopup: React.FC<MapWithPopupProps> = ({
   );
 
   const [searchInput, setSearchInput] = useState('');
-
-  const [mapTypeId, setMapTypeId] = useState<string>('roadmap');
 
   useEffect(() => {
     setMapElement(mapRef.current);
