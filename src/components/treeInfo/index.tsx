03fe1edada24b27852ddd71fd7e14f2f11113649
Kenny Jung <dukyoung01@naver.com>
2023-04-03 21:50:19 -0400

    Implement force unadopt

--- src/components/treeInfo/index.tsx
+++ src/components/treeInfo/index.tsx
@@ -34,6 +34,10 @@ const UnadoptButton = styled(Button)`
   }
 `;
 
+const ForceUnadoptButton = styled(Button)`
+  margin: 10px;
+`;
+
 interface TreeProps {
   readonly siteData: SiteProps;
   readonly loggedIn: boolean;
@@ -41,6 +45,7 @@ interface TreeProps {
   readonly mobile?: boolean;
   readonly onClickAdopt: () => void;
   readonly onClickUnadopt: () => void;
+  readonly onClickForceUnadopt: () => void;
   readonly onFinishRecordStewardship: (
     values: RecordStewardshipRequest,
   ) => void;
@@ -56,6 +61,7 @@ const TreeInfo: React.FC<TreeProps> = ({
   mobile,
   onClickAdopt,
   onClickUnadopt,
+  onClickForceUnadopt,
   onFinishRecordStewardship,
   stewardshipFormInstance,
   editTreeNameFormInstance,
@@ -64,7 +70,7 @@ const TreeInfo: React.FC<TreeProps> = ({
   const history = useHistory();
   const location = useLocation<RedirectStateProps>();
 
-  const adopted = siteData.entries[0] && siteData.entries[0].adopter !== null;
+  const isAdopted = !!siteData.entries?.[0]?.adopter;
 
   const userIsAdmin: boolean = useSelector((state: C4CState) =>
     isAdmin(state.authenticationState.tokens),
@@ -90,7 +96,7 @@ const TreeInfo: React.FC<TreeProps> = ({
       defaultText={
         userOwnsTree
           ? 'Check out this tree I adopted!'
-          : adopted
+          : isAdopted
           ? 'Check out this tree near you!'
           : `This tree${
               siteData.address ? ' at ' + siteData.address : ''
@@ -125,67 +131,73 @@ const TreeInfo: React.FC<TreeProps> = ({
         }
       </TreeHeader>
 
-      {(() => {
-        switch (loggedIn) {
-          case true:
-            return (
-              <>
-                {userOwnsTree ? (
-                  <>
-                    <UnadoptButton
-                      danger
-                      size={mobile ? 'middle' : 'large'}
-                      onClick={onClickUnadopt}
-                    >
-                      Unadopt
-                    </UnadoptButton>
-                    {shareButton}
-                    <StewardshipContainer>
-                      <Typography.Title level={3}>
-                        Record your tree care activity below.
-                      </Typography.Title>
-                      <StewardshipForm
-                        onFinish={onFinishRecordStewardship}
-                        form={stewardshipFormInstance}
-                      />
-                    </StewardshipContainer>
-                  </>
-                ) : (
-                  <>
-                    <Button
-                      type="primary"
-                      size={mobile ? 'middle' : 'large'}
-                      onClick={onClickAdopt}
-                      disabled={adopted}
-                    >
-                      {adopted ? 'Already Adopted' : 'Adopt'}
-                    </Button>
-                    {shareButton}
-                  </>
-                )}
-              </>
-            );
-          case false:
-            return (
-              <>
-                <ToggleTextButton
-                  type="link"
-                  size="large"
-                  onClick={() =>
-                    history.push(Routes.LOGIN, {
-                      destination: location.pathname,
-                    })
-                  }
-                >
-                  Log in to adopt this tree!
-                </ToggleTextButton>
-                {shareButton}
-              </>
-            );
-        }
-      })()}
+      {loggedIn ? (
+        <>
+          {userOwnsTree ? (
+            <UnadoptButton
+              danger
+              size={mobile ? 'middle' : 'large'}
+              onClick={onClickUnadopt}
+            >
+              Unadopt
+            </UnadoptButton>
+          ) : (
+            <Button
+              type="primary"
+              size={mobile ? 'middle' : 'large'}
+              onClick={onClickAdopt}
+              disabled={isAdopted}
+            >
+              {isAdopted ? 'Already Adopted' : 'Adopt'}
+            </Button>
+          )}
+
+          {userIsAdmin && (
+            <ForceUnadoptButton
+              danger
+              size={mobile ? 'middle' : 'large'}
+              onClick={onClickForceUnadopt}
+              disabled={!isAdopted}
+            >
+              Force Unadopt
+            </ForceUnadoptButton>
+          )}
+
+          {shareButton}
+
+          {userOwnsTree && (
+            <StewardshipContainer>
+              <Typography.Title level={3}>
+                Record your tree care activity below.
+              </Typography.Title>
+              <StewardshipForm
+                onFinish={onFinishRecordStewardship}
+                form={stewardshipFormInstance}
+              />
+            </StewardshipContainer>
+          )}
+        </>
+      ) : (
+        <>
+          <ToggleTextButton
+            type="link"
+            size="large"
+            onClick={() =>
+              history.push(Routes.LOGIN, {
+                destination: location.pathname,
+              })
+            }
+          >
+            Log in to adopt this tree!
+          </ToggleTextButton>
+          {shareButton}
+        </>
+      )}
     </>
   );
 };
 
 export default TreeInfo;
+function dispatch(arg0: any) {
+  throw new Error('Function not implemented.');
+}
