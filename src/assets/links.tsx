4795e67c373412515a5c97a6128abb8e15fb4bb4
Kenny Jung <33704204+chromium-52@users.noreply.github.com>
2023-04-07 00:52:33 -0400

    Kjung/feat cambridge override (#217)
    * Move site constant and start adding Cambridge stuff
    
    * Add Cambridge banner and revert overriding some colors

--- src/assets/links.tsx
+++ src/assets/links.tsx
@@ -1,5 +1,8 @@
-export const SFTT_PARTNER_LOGOS =
-  'https://d2j3fegnzkmagm.cloudfront.net/sftt-partners.png';
+import { Websites, site } from '../constants';
+
+export const SFTT_PARTNER_LOGOS = `https://d2j3fegnzkmagm.cloudfront.net/${
+  site === Websites.CAMBRIDGE ? 'cambridge' : 'sftt'
+}-partners.png`;
 
 export const CITY_PLANTING_REQUEST_LINK =
   'https://www.cityofboston.gov/mayor/24?topic=new-tree-request';
