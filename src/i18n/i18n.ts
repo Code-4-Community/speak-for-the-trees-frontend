6e07a0ccd9c2eb7df9f1134bbfc5d14fa62b131b
Avery Huang <averyhuang2003@gmail.com>
2024-01-28 00:27:40 -0500

    switch back to default en

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -116,7 +116,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    lng: 'es', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
     fallbackLng: 'en',
