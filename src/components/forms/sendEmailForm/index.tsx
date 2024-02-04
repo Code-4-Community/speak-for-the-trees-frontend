b1f1ad7f8ac5ce04be88b66d5491e80547bb2ace
Avery Huang <averyhuang2003@gmail.com>
2024-02-04 12:56:51 -0500

    touchup + translations

--- src/components/forms/sendEmailForm/index.tsx
+++ src/components/forms/sendEmailForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Form, Input, Switch, message } from 'antd';
 import { SubmitButton } from '../../../components/themedComponents';
@@ -8,23 +8,27 @@ import {
   SendEmailRequest,
 } from '../../../components/forms/ducks/types';
 import { requiredRule } from '../../../utils/formRules';
+import { site } from '../../../constants';
+import { useTranslation } from 'react-i18next';
+import { n } from '../../../utils/stringFormat';
 import DOMPurify from 'isomorphic-dompurify';
 
 const PreviewSwitch = styled(Switch)`
   display: flex;
   align-items: center;
-  // min-width: 80px;
   margin-bottom: 10px;
   padding: 10px 5px;
 `;
 
 const EmailPreview = styled.div`
-  min-height: 200px;
+  min-height: 250px;
   margin-bottom: 24px;
-  max-height: 400px;
+  max-height: 800px;
   border: 1px solid #d9d9d9;
   border-radius: 4px;
   padding: 3px 11px;
+  resize: vertical;
+  overflow-y: scroll; // required for resizing
 `;
 
 interface SendEmailFormProps {
@@ -32,16 +36,28 @@ interface SendEmailFormProps {
 }
 
 const SendEmailForm: React.FC<SendEmailFormProps> = ({ emails }) => {
+  const { t } = useTranslation(n(site, ['forms']), {
+    keyPrefix: 'volunteer_emailer',
+    nsMode: 'fallback',
+  });
+
   const [sendEmailForm] = Form.useForm();
 
   const [showPreview, setShowPreview] = useState<boolean>(false);
   const [bodyContent, setBodyContent] = useState<string>('');
-  const sanitizedBodyContent = useMemo(
-    () => DOMPurify.sanitize(bodyContent),
-    [bodyContent],
-  );
+  const [sanitizedBodyContent, setSanitizedBodyContent] = useState<string>('');
+
+  const switchView = (isShowPreview: boolean) => {
+    setShowPreview(isShowPreview);
+    isShowPreview && setSanitizedBodyContent(DOMPurify.sanitize(bodyContent));
+  };
 
   const onFinishSendEmail = (values: SendEmailFormValues) => {
+    if (emails.length === 0) {
+      message.error(t('users_required'));
+      return;
+    }
+
     const sendEmailRequest: SendEmailRequest = {
       ...values,
       emails,
@@ -49,10 +65,10 @@ const SendEmailForm: React.FC<SendEmailFormProps> = ({ emails }) => {
 
     ProtectedApiClient.sendEmail(sendEmailRequest)
       .then(() => {
-        message.success('Email sent!');
+        message.success(t('success'));
       })
       .catch((err) =>
-        message.error(`Email could not be sent: ${err.response.data}`),
+        message.error(t('response_error', { error: err.response.data })),
       );
   };
 
@@ -68,29 +84,29 @@ const SendEmailForm: React.FC<SendEmailFormProps> = ({ emails }) => {
     >
       <Form.Item
         name="emailSubject"
-        rules={requiredRule('The email subject is required')}
+        rules={requiredRule(t('subject_required'))}
       >
-        <Input placeholder="Email Subject" />
+        <Input placeholder={t('subject_placeholder')} />
       </Form.Item>
       <PreviewSwitch
-        onChange={(checked) => setShowPreview(checked)}
-        checkedChildren="Preview"
-        unCheckedChildren="Raw"
+        onChange={switchView}
+        checkedChildren={t('preview.preview')}
+        unCheckedChildren={t('preview.raw')}
       />
-      {showPreview ? (
+      <Form.Item
+        name="emailBody"
+        rules={requiredRule(t('body_required'))}
+        hidden={showPreview}
+      >
+        <Input.TextArea rows={8} placeholder={t('body_placeholder')} />
+      </Form.Item>
+      {showPreview && (
         <EmailPreview
           dangerouslySetInnerHTML={{ __html: sanitizedBodyContent }}
         />
-      ) : (
-        <Form.Item
-          name="emailBody"
-          rules={requiredRule('The email body is required')}
-        >
-          <Input.TextArea rows={6} placeholder="Email Body" />
-        </Form.Item>
       )}
       <SubmitButton type="primary" htmlType="submit">
-        Send Email
+        {t('send')}
       </SubmitButton>
     </Form>
   );
